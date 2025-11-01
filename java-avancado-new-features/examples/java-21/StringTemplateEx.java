125e15aab069f53724ab5b52065a881ad20e6ef3
Gerry <gerry.souza-ext@ab-inbev.com>
2025-11-01 12:48:26 -0300

    79. String Templates

---
+++ java-avancado-new-features/examples/java-21/StringTemplateEx.java
@@ -0,0 +1,15 @@
+
+public class StringTemplateEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String curso = "Java class";
+		
+		String saudacao = STR."Welcome to \{curso}";
+		
+		System.out.println(saudacao);
+
+	}
+
+}
